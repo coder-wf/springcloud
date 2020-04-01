4ad22f54607d9de05109acfb152455c881620135
wf <2661159674@qq.com>
2020-04-01 17:15:30 +0800

    learn springcloud

--- product-view-service-feign/src/main/java/com/wyt/springcloud/web/ProductController.java
+++ product-view-service-feign/src/main/java/com/wyt/springcloud/web/ProductController.java
@@ -3,6 +3,7 @@ import java.util.List;
  
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.wyt.springcloud.pojo.Product;
 import com.wyt.springcloud.service.ProductService;
   
 @Controller
+@RefreshScope
 public class ProductController {
   
     @Autowired ProductService productService;
